e074949e8a61ba2aa50cc7b2788edc73c5f85598
Vitaliy Makeev <w.makeev@gmail.com>
2020-04-14 07:21:30 +0500

    ✏️ doc(examples): add example

---
+++ examples/query.js
@@ -0,0 +1,18 @@
+const { URL } = require('url')
+const assert = require('assert')
+const Moysklad = require('moysklad')
+
+const ms = Moysklad({ apiVersion: '1.2' })
+
+const getQueryStrParamValue = (url, param) =>
+  new URL(url).searchParams.get(param)
+
+// query.order
+const orderQueryUrl = ms.buildUrl('entity/customerorder', {
+  order: ['name,desc', ['code', 'asc'], ['moment']]
+})
+
+assert.equal(
+  getQueryStrParamValue(orderQueryUrl, 'order'),
+  'name,desc;code,asc;moment'
+)

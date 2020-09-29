0897ad0b41eca9c7fb4ef14ce8ce35fb173f240f
Vitaliy Makeev <w.makeev@gmail.com>
2020-09-29 16:06:54 +0500

    feat(getEnvVar): значение по умолчанию

--- src/getEnvVar.js
+++ src/getEnvVar.js
@@ -1,6 +1,12 @@
 'use strict'
 
-function getEnvVar (key) {
+/**
+ * Получить значение переменной окружения
+ * @param {string} key Наименоване переменной окружения
+ * @param {string} defaultValue Наименоване переменной окружения
+ * @returns {string | null} Значение переменной окружения
+ */
+function getEnvVar (key, defaultValue) {
   if (typeof process !== 'undefined' && process.env && process.env[key]) {
     return process.env[key]
   } else if (typeof window !== 'undefined' && window[key] != null) {
@@ -8,7 +14,7 @@ function getEnvVar (key) {
   } else if (typeof global !== 'undefined' && global[key] != null) {
     return global[key]
   } else {
-    return null
+    return defaultValue !== undefined ? defaultValue : null
   }
 }
 
